57420bd18a052ebac16b9ce71756224594fd3fb0
lovemilk <zhuhansan666@outlook.com>
2024-09-13 23:20:55 +0800

    add: build.py

---
+++ build.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from subprocess import run
+
+input_dir = Path('mffm11_v2024.04.04_HarmonyOSSans/*')
+output = Path('./dist/mffm11_v2024.04.04_HarmonyOSSans.zip')
+
+def get_version():
+    with (input_dir / 'module.prop').open('r', encoding='u8') as fp:
+        while line := fp.readline():
+            if not line.startswith('version='):
+                continue
+
+            data = line.split('=', 1)[1].strip()
+            return data.split('-', 1)[0] if '-' in data else data
+
+def build():
+    output.unlink(missing_ok=True)
+
+    process = run([
+        '7z',
+        'a',
+        str(output.absolute()),
+        str(input_dir.absolute()),
+    ])
+    assert process.returncode == 0, 'failed to create zip file'
+
+def release():
+    version = get_version()
+    process = run([
+        'gh',
+        'release',
+        'create',
+        f'V{version}',
+        '--title',
+        f'V{version}',
+        str(output.absolute()),
+    ])
+
+    assert process.returncode == 0, 'failed to create release'
+
+if __name__ == '__main__':
+    build()
